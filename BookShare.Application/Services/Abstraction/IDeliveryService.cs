b632842ba6c1c5e8fa4f04396b6b90e1d832bd69
olusheyi <boboladvat@gmail.com>
2023-12-07 15:18:30 +0100

    Added DeliveryService methods

--- BookShare.Application/Services/Abstraction/IDeliveryService.cs
+++ BookShare.Application/Services/Abstraction/IDeliveryService.cs
@@ -5,13 +5,13 @@ namespace BookShare.Application.Services.Abstraction
     public interface IDeliveryService
     {
         Task<DeliveryDto> CreateDelivery(DeliveryCreationDto deliveryCreationDto);
-        Task DeleteDelivery(string deliveryId);
+        Task DeleteDelivery(string deliveryId, bool trackChanges);
         Task<IEnumerable<DeliveryDto>> GetAllDeliveriesAsync(bool trackChanges);
         Task<IEnumerable<DeliveryDto>> GetAllDeliveriesByRequestId(string requestId, bool trackChanges);
         Task<IEnumerable<DeliveryDto>> GetAllDeliveriesByTransporterId(string transporterId, bool trackChanges);
         Task<IEnumerable<DeliveryDto>> GetAllDeliveriesByUserId(string  userId, bool trackChanges);
         Task<DeliveryDto> GetDeliveryByDeliveryId(string deliveryId, bool trackChanges);
-        Task UpdateDelivery(DeliveryUpdateDto deliveryUpdateDto);
+        Task UpdateDelivery(string deliveryId, DeliveryUpdateDto deliveryUpdateDto, bool trackChanges);
 
     }
 }
