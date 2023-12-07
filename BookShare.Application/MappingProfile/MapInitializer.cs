7e26b397a27ef43c95d6c293bae83aed679e037b
Marvelous-iAcademy <130442240+Marvelous-iAcademy@users.noreply.github.com>
2023-12-07 15:22:48 +0000

    User Repository and Services

--- BookShare.Application/MappingProfile/MapInitializer.cs
+++ BookShare.Application/MappingProfile/MapInitializer.cs
@@ -1,13 +1,9 @@
 ﻿using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BookShare.Application.MappingProfile
 {
     public class MapInitializer : Profile
     {
+       
     }
 }
---
+++ BookShare.Common/Dto/Response/UserResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShare.Common.Dto.Response
+{
+    public class UserResponseDto
+    {
+        public string UserId { get; set; } = Guid.NewGuid().ToString();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ImageUrl { get; set; }
+        public string IsVerified { get; set; }
+    }
+}
