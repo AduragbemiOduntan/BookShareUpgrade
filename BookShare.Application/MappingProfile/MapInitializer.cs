0327dca67eb9e7c74d6773ad654e16b6975aa494
Victor <ndulue.victor.c@gmail.com>
2023-12-07 19:50:16 +0100

    .

--- BookShare.Application/MappingProfile/MapInitializer.cs
+++ BookShare.Application/MappingProfile/MapInitializer.cs
@@ -2,24 +2,18 @@
 using BookShare.Common.Dto.Request;
 using BookShare.Common.Dto.Response;
 using BookShare.Domain.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BookShare.Application.MappingProfile
+namespace BookShare.Application.MappingProfile;
+
+public class MapInitializer : Profile
 {
-    public class MapInitializer : Profile
+    public MapInitializer()
     {
-        public MapInitializer()
-        {
-            CreateMap<Request, RequestResponseDto>().ForMember(dest => dest.Books, opts => opts.MapFrom(src => src.Books));
-            CreateMap<DeliveryCreationDto, Delivery>();
-            CreateMap<DeliveryUpdateDto, Delivery>();
-            CreateMap<Delivery, DeliveryDto>();
-            CreateMap<UserSignUpRequestDto, User>();
-            CreateMap<User, UserResponseDto>();
-        }
+        CreateMap<Request, RequestResponseDto>().ForMember(dest => dest.Books, opts => opts.MapFrom(src => src.Books));
+        CreateMap<DeliveryCreationDto, Delivery>();
+        CreateMap<DeliveryUpdateDto, Delivery>();
+        CreateMap<Delivery, DeliveryDto>();
+        CreateMap<UserSignUpRequestDto, User>();
+        CreateMap<User, UserResponseDto>();
     }
 }
