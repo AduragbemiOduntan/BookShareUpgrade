f09269e4ef67f302cb2b4ced47391e9c3e74a24f
Marvelous-iAcademy <130442240+Marvelous-iAcademy@users.noreply.github.com>
2023-12-07 16:58:17 +0000

    Updated User Controller

--- BookShare.Common/Dto/Request/UserCreateRequestDto.cs
+++ BookShare.Common/Dto/Request/UserCreateRequestDto.cs
@@ -8,10 +8,8 @@ namespace BookShare.Common.Dto.Request
 {
     public record UserCreateRequestDto
     {
-        public string UserId { get; set; } = Guid.NewGuid().ToString();
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string ImageUrl { get; set; }
         public string IsVerified { get; set; }
     }
 }
--- BookShare.Infrastructure/Migrations/20231207163402_init.Designer.cs
+++ BookShare.Infrastructure/Migrations/20231207163402_init.Designer.cs
@@ -4,6 +4,7 @@ using BookShare.Infrastructure.ApplicationContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookShare.Infrastructure.Migrations
 {
     [DbContext(typeof(BookShareContext))]
-    partial class BookShareContextModelSnapshot : ModelSnapshot
+    [Migration("20231207163402_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
